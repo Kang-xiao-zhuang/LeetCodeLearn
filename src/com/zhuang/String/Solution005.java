1e9c60506e77fb6b29c852df2631fee788768b41
ZhuangKang <2247830091@qq.com>
2021-08-16 21:16:30 +0800

    字符串

--- src/com/zhuang/String/Solution005.java
+++ src/com/zhuang/String/Solution005.java
@@ -9,8 +9,11 @@ package com.zhuang.String;
 public class Solution005 {
     public static void main(String[] args) {
         String s = "A man, a plan, a canal: Panama";
-        isPalindrome(s);
-        isPalindrome2(s);
+        // isPalindrome(s);
+        //   isPalindrome2(s);
+
+        //  countAndSay(4);
+        countAndSay2(4);
     }
 
     /**
@@ -61,5 +64,62 @@ public class Solution005 {
         }
         System.out.println(true);
         return true;
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/count-and-say/
+     * 循环法
+     *
+     * @param n 正整数
+     * @return 字符串
+     */
+    public static String countAndSay(int n) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(1);
+        for (int i = 1; i < n; i++) {
+            // 记录当前的字符串
+            StringBuilder s = new StringBuilder();
+            // 记录每个数字的开始索引
+            int start = 0;
+            for (int j = 1; j < sb.length(); j++) {
+                // 当数字发生改变时执行
+                if (sb.charAt(j) != sb.charAt(start)) {
+                    s.append(j - start).append(sb.charAt(start));
+                    start = j;
+                }
+            }
+            // 字符串最后一个数字
+            s.append(sb.length() - start).append(sb.charAt(start));
+            sb = s;
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 递归法
+     *
+     * @param n 正整数
+     * @return 字符串
+     */
+    public static String countAndSay2(int n) {
+        // 递归终止条件
+        if (n == 1) {
+            return "1";
+        }
+        // 获取到上一层的字符串
+        String s = countAndSay2(n - 1);
+        StringBuilder result = new StringBuilder();
+        // 记录每个数字的开始索引
+        int start = 0;
+        for (int i = 1; i < s.length(); i++) {
+            // 当数字发生改变时执行
+            if (s.charAt(i) != s.charAt(start)) {
+                result.append(i - start).append(s.charAt(start));
+                start = i;
+            }
+        }
+        // 字符串最后一个数字
+        result.append(s.length() - start).append(s.charAt(start));
+        return result.toString();
     }
 }
