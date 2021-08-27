96d469afc71d295ecfcb6c49caee81b2f804ac4a
KangXiaoZhuang <2247830091@qq.com>
2021-08-27 15:28:53 +0800

    数组

--- src/com/zhuang/Array/Solution013.java
+++ src/com/zhuang/Array/Solution013.java
@@ -14,6 +14,12 @@ public class Solution013 {
         //triangleNumber(nums);
         //triangleNumber2(nums);
         //triangleNumber3(nums);
+
+
+        int[][] images = {{1, 1, 0}, {1, 0, 1}, {0, 0, 0}};
+        //flipAndInvertImage(images);
+        //flipAndInvertImage2(images);
+
     }
 
 
@@ -92,6 +98,65 @@ public class Solution013 {
             }
         }
         System.out.println(ans);
+        return ans;
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/flipping-an-image/
+     * 双指针法
+     *
+     * @param image 二维数组
+     * @return 二维数组
+     */
+    public static int[][] flipAndInvertImage(int[][] image) {
+        int n = image.length;
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                if (image[i][left] == image[i][right]) {
+                    image[i][left] ^= 1;
+                    image[i][right] ^= 1;
+                }
+                left++;
+                right--;
+            }
+            if (left == right) {
+                image[i][left] ^= 1;
+            }
+        }
+        System.out.println(Arrays.deepToString(image));
+        return image;
+    }
+
+    /**
+     * 复制数组
+     *
+     * @param image 二维数组
+     * @return 二维数组
+     */
+    public static int[][] flipAndInvertImage2(int[][] image) {
+        int n = image.length;
+        int[][] ans = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            // 对每⼀⾏进⾏拷⻉（共三种⽅式）
+            // ans[i] = a[i].clone();
+            // ans[i] = Arrays.copyOf(a[i], n);
+            System.arraycopy(image[i], 0, ans[i], 0, n);
+            int left = 0, right = n - 1;
+            // 交换实现翻转，异或实现反转
+            while (left < right) {
+                int temp = ans[i][right];
+                ans[i][right] = ans[i][left] ^ 1;
+                ans[i][left] = temp ^ 1;
+                left++;
+                right--;
+            }
+            // 中间特殊处理
+            if (n % 2 != 0) {
+                ans[i][left] ^= 1;
+            }
+        }
+        System.out.println(Arrays.deepToString(ans));
         return ans;
     }
 }
