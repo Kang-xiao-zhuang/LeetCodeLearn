326e1e71b2bd214d98ea5ce7f94fedf6b321094d
KangXiaoZhuang <2247830091@qq.com>
2021-09-02 09:25:10 +0800

    链表

---
+++ src/com/zhuang/Daily/September/ListNode.java
@@ -0,0 +1,16 @@
+package com.zhuang.Daily.September;
+
+/**
+ * @Classname ListNode
+ * @Description 链表节点类
+ * @Date 2021/9/2 9:23
+ * @Author by Zhuang
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
