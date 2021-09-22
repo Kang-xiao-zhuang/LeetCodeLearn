eb9afb65cacfce22af45405cdbff33f7801aa9c3
KangXiaoZhuang <2247830091@qq.com>
2021-09-22 09:25:22 +0800

    9.22每日一题

--- src/com/zhuang/Daily/September/Solution003.java
+++ src/com/zhuang/Daily/September/Solution003.java
@@ -296,4 +296,36 @@ public class Solution003 {
         }
         return len;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/split-linked-list-in-parts/
+     * 9.22
+     *
+     * @param head 头节点
+     * @param k    整数k
+     * @return 分隔k个连续的部分
+     */
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int n = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            n++;
+            temp = temp.next;
+        }
+        int quotient = n / k, remainder = n % k;
+
+        ListNode[] parts = new ListNode[k];
+        ListNode curr = head;
+        for (int i = 0; i < k && curr != null; i++) {
+            parts[i] = curr;
+            int partSize = quotient + (i < remainder ? 1 : 0);
+            for (int j = 1; j < partSize; j++) {
+                curr = curr.next;
+            }
+            ListNode next = curr.next;
+            curr.next = null;
+            curr = next;
+        }
+        return parts;
+    }
 }
