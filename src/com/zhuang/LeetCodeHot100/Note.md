3f5504ba2af0d1148380de60ea4f8e3f1c19f07a
KangXiaoZhuang <2247830091@qq.com>
2021-09-23 11:24:11 +0800

    LeetCodeHot100

--- src/com/zhuang/LeetCodeHot100/Note.md
+++ src/com/zhuang/LeetCodeHot100/Note.md
@@ -1121,3 +1121,98 @@ class Solution {
 }
 ```
 
+#### [31. 下一个排列](https://leetcode-cn.com/problems/next-permutation/)
+
+实现获取 **下一个排列** 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列（即，组合出下一个更大的整数）。
+
+如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+
+必须**[ 原地 ](https://baike.baidu.com/item/原地算法)**修改，只允许使用额外常数空间。
+
+ 
+
+**示例 1：**
+
+```
+输入：nums = [1,2,3]
+输出：[1,3,2]
+```
+
+**示例 2：**
+
+```
+输入：nums = [3,2,1]
+输出：[1,2,3]
+```
+
+**示例 3：**
+
+```
+输入：nums = [1,1,5]
+输出：[1,5,1]
+```
+
+**示例 4：**
+
+```
+输入：nums = [1]
+输出：[1]
+```
+
+ 
+
+**提示：**
+
+- `1 <= nums.length <= 100`
+- `0 <= nums[i] <= 100`
+
+**两边扫描**
+
+```java
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        // 是否为降序排列
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    /**
+     * 交换函数
+     *
+     * @param nums 数组
+     * @param i    数1
+     * @param j    数2
+     */
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    /**
+     * 反转数组元素
+     *
+     * @param nums  数组
+     * @param start 开始的索引
+     */
+    public static void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
+```
+
--- src/com/zhuang/LeetCodeHot100/Solution02.java
+++ src/com/zhuang/LeetCodeHot100/Solution02.java
@@ -29,9 +29,10 @@ public class Solution02 {
 
 
         //generateParenthesis(3);
-        generateParenthesis2(3);
+        //generateParenthesis2(3);
+        //int[] nums = {1, 2, 3};
+        //nextPermutation(nums);
     }
-
 
     /**
      * https://leetcode-cn.com/problems/container-with-most-water/
@@ -361,7 +362,6 @@ public class Solution02 {
         return stack.isEmpty();
     }
 
-
     static List<String> res = new ArrayList<String>();
 
     /**
@@ -448,5 +448,56 @@ public class Solution02 {
             }
         }
         return balance == 0;
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/next-permutation/
+     * 第31题
+     *
+     * @param nums 数组
+     */
+    public static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        // 是否为降序排列
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    /**
+     * 交换函数
+     *
+     * @param nums 数组
+     * @param i    数1
+     * @param j    数2
+     */
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    /**
+     * 反转数组元素
+     *
+     * @param nums  数组
+     * @param start 开始的索引
+     */
+    public static void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
     }
 }
