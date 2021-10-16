b128eb6a7355114fe38fef4768b0eb0d4ae233c0
ZhuangKang <2247830091@qq.com>
2021-10-17 07:29:54 +0800

    TreeNode类

---
+++ src/com/zhuang/Daily/October/TreeNode.java
@@ -0,0 +1,27 @@
+package com.zhuang.Daily.October;
+
+/**
+ * @Classname TreeNode
+ * @Description TreeNode
+ * @Date 2021/10/17 7:22
+ * @Author by dell
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
