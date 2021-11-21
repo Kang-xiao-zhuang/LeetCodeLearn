1adda63fff68923e3242124e0dd0956188808ea5
ZhuangKang <2247830091@qq.com>
2021-11-21 12:46:03 +0800

    Node类

---
+++ src/com/zhuang/Daily/November/Node.java
@@ -0,0 +1,26 @@
+package com.zhuang.Daily.November;
+
+import java.util.List;
+
+/**
+ * @Classname Node
+ * @Description 用一句话描述类的作用
+ * @Date 2021/11/21 12:24
+ * @Author by dell
+ */
+
+public class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
