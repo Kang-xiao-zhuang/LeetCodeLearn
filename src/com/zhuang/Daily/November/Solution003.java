477e54f5db5c61881b897a4837c7010b96d15cd5
KangXiaoZhuang <2247830091@qq.com>
2021-11-23 09:20:51 +0800

    10.23每日一题

--- src/com/zhuang/Daily/November/Solution003.java
+++ src/com/zhuang/Daily/November/Solution003.java
@@ -278,4 +278,42 @@ public class Solution003 {
             return nums;
         }
     }
+
+    /**
+     * https://leetcode-cn.com/problems/buddy-strings/
+     * 11.23
+     *
+     * @param s    字符串
+     * @param goal 字符串
+     * @return 是否为亲密字符串
+     */
+    public boolean buddyStrings(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        if (s.equals(goal)) {
+            int[] count = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
+                if (count[s.charAt(i) - 'a'] > 1) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            int first = -1, second = -1;
+            for (int i = 0; i < goal.length(); i++) {
+                if (s.charAt(i) != goal.charAt(i)) {
+                    if (first == -1) {
+                        first = i;
+                    } else if (second == -1) {
+                        second = i;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return (second != -1 && s.charAt(first) == goal.charAt(second) && s.charAt(second) == goal.charAt(first));
+        }
+    }
 }
