e8310b1f6560a8559728bca75653f56dd81b6def
KangXiaoZhuang <2247830091@qq.com>
2021-11-24 16:58:12 +0800

    11.24每日一题

--- src/com/zhuang/Daily/November/Solution003.java
+++ src/com/zhuang/Daily/November/Solution003.java
@@ -18,7 +18,9 @@ public class Solution003 {
         //maxProduct(words);
         int[] nums = {1, 3, 2, 2, 5, 2, 3, 7};
         //findLHS(nums);
-        findLHS2(nums);
+        //findLHS2(nums);
+
+        originalDigits("owoztneoer");
     }
 
     /**
@@ -315,5 +317,42 @@ public class Solution003 {
             }
             return (second != -1 && s.charAt(first) == goal.charAt(second) && s.charAt(second) == goal.charAt(first));
         }
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/reconstruct-original-digits-from-english/
+     * 11.24
+     *
+     * @param s 字符串
+     * @return 升序返回原始的数字
+     */
+    public static String originalDigits(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int[] cnt = new int[10];
+        cnt[0] = map.getOrDefault('z', 0);
+        cnt[2] = map.getOrDefault('w', 0);
+        cnt[4] = map.getOrDefault('u', 0);
+        cnt[6] = map.getOrDefault('x', 0);
+        cnt[8] = map.getOrDefault('g', 0);
+
+        cnt[3] = map.getOrDefault('h', 0) - cnt[8];
+        cnt[5] = map.getOrDefault('f', 0) - cnt[4];
+        cnt[7] = map.getOrDefault('s', 0) - cnt[6];
+
+        cnt[1] = map.getOrDefault('o', 0) - cnt[0] - cnt[2] - cnt[4];
+        cnt[9] = map.getOrDefault('i', 0) - cnt[5] - cnt[6] - cnt[8];
+
+        StringBuffer ans = new StringBuffer();
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < cnt[i]; ++j) {
+                ans.append((char) (i + '0'));
+            }
+        }
+        System.out.println(ans.toString());
+        return ans.toString();
     }
 }
