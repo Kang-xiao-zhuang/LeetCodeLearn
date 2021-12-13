c11c2fad74d4d5e34b8b7f7b78c37eca998b78eb
KangXiaoZhuang <2247830091@qq.com>
2021-12-13 10:58:46 +0800

    12.13每日一题

--- src/com/zhuang/Daily/December/Solution002.java
+++ src/com/zhuang/Daily/December/Solution002.java
@@ -16,8 +16,10 @@ public class Solution002 {
         //String[] board = {"XOX", "O O", "XOX"};
         //validTicTacToe(board);
 
-        String[] words = {"step", "steps", "stripe", "stepple"};
-        shortestCompletingWord("1s3 PSt", words);
+        //String[] words = {"step", "steps", "stripe", "stepple"};
+        //shortestCompletingWord("1s3 PSt", words);
+        int[][] grid={{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
+        maxIncreaseKeepingSkyline(grid);
     }
 
     /**
@@ -188,5 +190,32 @@ public class Solution002 {
      */
     public String toLowerCase(String s) {
         return s.toLowerCase();
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/max-increase-to-keep-city-skyline/
+     * 12.13
+     *
+     * @param grid 二维数组
+     * @return 最大总和
+     */
+    public static int maxIncreaseKeepingSkyline(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int[] rowMax = new int[n];
+        int[] colMax = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rowMax[i] = Math.max(rowMax[i], grid[i][j]);
+                colMax[j] = Math.max(colMax[j], grid[i][j]);
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                res += Math.min(rowMax[i], colMax[j]) - grid[i][j];
+            }
+        }
+        System.out.println(res);
+        return res;
     }
 }
