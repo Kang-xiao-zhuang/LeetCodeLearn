041afbaed4394e4730cb1cf86ccc3816bc853092
KangXiaoZhuang <2247830091@qq.com>
2021-12-16 11:14:19 +0800

    12.16每日一题

--- src/com/zhuang/Daily/December/Solution003.java
+++ src/com/zhuang/Daily/December/Solution003.java
@@ -53,4 +53,39 @@ public class Solution003 {
         }
         return ans;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/maximum-number-of-visible-points/
+     * 12.16
+     *
+     * @param points   数组
+     * @param angle    整数
+     * @param location 位置
+     * @return 看到的点的最大数目
+     */
+    public int visiblePoints(List<List<Integer>> points, int angle, List<Integer> location) {
+        int x = location.get(0), y = location.get(1);
+        List<Double> list = new ArrayList<>();
+        int cnt = 0;
+        double pi = Math.PI, t = angle * pi / 180;
+        for (List<Integer> p : points) {
+            int a = p.get(0), b = p.get(1);
+            if (a == x && b == y && ++cnt >= 0) {
+                continue;
+            }
+            list.add(Math.atan2(b - y, a - x));
+        }
+        Collections.sort(list);
+        int n = list.size(), max = 0;
+        for (int i = 0; i < n; i++) {
+            list.add(list.get(i) + 2 * pi);
+        }
+        for (int i = 0, j = 0; j < 2 * n; j++) {
+            while (i < j && list.get(j) - list.get(i) > t) {
+                i++;
+            }
+            max = Math.max(max, j - i + 1);
+        }
+        return cnt + max;
+    }
 }
