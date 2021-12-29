4493b392c56908ea98ccde26de449d9b62e78fb4
KangXiaoZhuang <2247830091@qq.com>
2021-12-29 14:31:08 +0800

    12.29每日一题

--- src/com/zhuang/Daily/December/Solution004.java
+++ src/com/zhuang/Daily/December/Solution004.java
@@ -176,4 +176,28 @@ public class Solution004 {
         }
         return false;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/count-special-quadruplets/
+     * 12.29
+     *
+     * @param nums 数组
+     * @return 不同的四元组
+     */
+    public int countQuadruplets(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for (int a = 0; a < n; ++a) {
+            for (int b = a + 1; b < n; ++b) {
+                for (int c = b + 1; c < n; ++c) {
+                    for (int d = c + 1; d < n; ++d) {
+                        if (nums[a] + nums[b] + nums[c] == nums[d]) {
+                            ++ans;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
 }
