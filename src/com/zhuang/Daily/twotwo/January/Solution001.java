f6f07eeb8ea515b0ab680ab528ad3c7dd6a0515d
ZhuangKang <2247830091@qq.com>
2022-01-03 10:01:06 +0800

    1.3每日一题

--- src/com/zhuang/Daily/twotwo/January/Solution001.java
+++ src/com/zhuang/Daily/twotwo/January/Solution001.java
@@ -49,4 +49,29 @@ public class Solution001 {
     public int lastRemaining(int n) {
         return n == 1 ? 1 : 2 * (n / 2 + 1 - lastRemaining(n / 2));
     }
+
+    /**
+     * https://leetcode-cn.com/problems/day-of-the-week/
+     *
+     * @param day   整数
+     * @param month 整数
+     * @param year  整数
+     * @return 对应一周中的哪一天
+     */
+    public String dayOfTheWeek(int day, int month, int year) {
+        String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+        /* 输入年份之前的年份的天数贡献 */
+        int days = 365 * (year - 1971) + (year - 1969) / 4;
+        /* 输入年份中，输入月份之前的月份的天数贡献 */
+        for (int i = 0; i < month - 1; ++i) {
+            days += monthDays[i];
+        }
+        if ((year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) && month >= 3) {
+            days += 1;
+        }
+        /* 输入月份中的天数贡献 */
+        days += day;
+        return week[(days + 3) % 7];
+    }
 }
