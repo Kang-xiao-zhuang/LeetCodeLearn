8d065514c4bb9a48c5e5dee84a77f0ffce0c041b
ZhuangKang <2247830091@qq.com>
2022-01-14 23:08:24 +0800

    1.14每日一题

--- src/com/zhuang/Daily/twotwo/January/Solution002.java
+++ src/com/zhuang/Daily/twotwo/January/Solution002.java
@@ -1,5 +1,9 @@
 package com.zhuang.Daily.twotwo.January;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
 /**
  * @Classname Solution002
  * @Description 2022.1.12-2022.1.22 每日一题
@@ -62,5 +66,36 @@ public class Solution002 {
             }
         }
         return m1 >= m2 * 2 ? index : -1;
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/find-k-pairs-with-smallest-sums/
+     *
+     * @param nums1 整数数组
+     * @param nums2 整数数组
+     * @param k     整数
+     * @return 最小的 k 个数对
+     */
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(k, (o1, o2) -> {
+            return nums1[o1[0]] + nums2[o1[1]] - nums1[o2[0]] - nums2[o2[1]];
+        });
+        List<List<Integer>> ans = new ArrayList<>();
+        int m = nums1.length;
+        int n = nums2.length;
+        for (int i = 0; i < Math.min(m, k); i++) {
+            pq.offer(new int[]{i, 0});
+        }
+        while (k-- > 0 && !pq.isEmpty()) {
+            int[] idxPair = pq.poll();
+            List<Integer> list = new ArrayList<>();
+            list.add(nums1[idxPair[0]]);
+            list.add(nums2[idxPair[1]]);
+            ans.add(list);
+            if (idxPair[1] + 1 < n) {
+                pq.offer(new int[]{idxPair[0], idxPair[1] + 1});
+            }
+        }
+        return ans;
     }
 }
