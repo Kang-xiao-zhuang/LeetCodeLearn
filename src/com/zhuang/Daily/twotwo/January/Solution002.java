eeb4eb9950d8d8eef02280c4abd3645101884947
ZhuangKang <2247830091@qq.com>
2022-01-15 08:22:44 +0800

    1.15每日一题

--- src/com/zhuang/Daily/twotwo/January/Solution002.java
+++ src/com/zhuang/Daily/twotwo/January/Solution002.java
@@ -70,6 +70,7 @@ public class Solution002 {
 
     /**
      * https://leetcode-cn.com/problems/find-k-pairs-with-smallest-sums/
+     * 1.14
      *
      * @param nums1 整数数组
      * @param nums2 整数数组
@@ -97,5 +98,26 @@ public class Solution002 {
             }
         }
         return ans;
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/calculate-money-in-leetcode-bank/
+     * 1.15
+     *
+     * @param n 天数
+     * @return 总价
+     */
+    public int totalMoney(int n) {
+        int week = 1, day = 1;
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            res += week + day - 1;
+            ++day;
+            if (day == 8) {
+                day = 1;
+                ++week;
+            }
+        }
+        return res;
     }
 }
