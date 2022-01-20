4ab0739180c13fe2becb7281e8ffb2bb9914af6d
KangXiaoZhuang <2247830091@qq.com>
2022-01-20 08:44:59 +0800

    1.20每日一题

--- src/com/zhuang/Daily/twotwo/January/Solution002.java
+++ src/com/zhuang/Daily/twotwo/January/Solution002.java
@@ -219,4 +219,29 @@ public class Solution002 {
         }
         return false;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/stone-game-ix/
+     * 1.20
+     *
+     * @param stones 石子
+     * @return 布尔
+     */
+    public boolean stoneGameIX(int[] stones) {
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        for (int val : stones) {
+            int type = val % 3;
+            if (type == 0) {
+                ++cnt0;
+            } else if (type == 1) {
+                ++cnt1;
+            } else {
+                ++cnt2;
+            }
+        }
+        if (cnt0 % 2 == 0) {
+            return cnt1 >= 1 && cnt2 >= 1;
+        }
+        return cnt1 - cnt2 > 2 || cnt2 - cnt1 > 2;
+    }
 }
