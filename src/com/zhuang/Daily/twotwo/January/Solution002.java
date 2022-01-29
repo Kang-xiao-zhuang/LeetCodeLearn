619b059d936479f3578abe7139ea5fd5b89bf8d1
KangXiaoZhuang <2247830091@qq.com>
2022-01-29 14:15:03 +0800

    1.29每日一题

--- src/com/zhuang/Daily/twotwo/January/Solution002.java
+++ src/com/zhuang/Daily/twotwo/January/Solution002.java
@@ -441,4 +441,36 @@ public class Solution002 {
         }
         return ans;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/map-of-highest-peak/
+     * 1.29
+     *
+     * @param isWater 整数矩阵
+     * @return 矩阵中的最高高度值 最大
+     */
+    public int[][] highestPeak(int[][] isWater) {
+        int m = isWater.length, n = isWater[0].length;
+        int[][] ans = new int[m][n];
+        Deque<int[]> d = new ArrayDeque<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isWater[i][j] == 1) d.addLast(new int[]{i, j});
+                ans[i][j] = isWater[i][j] == 1 ? 0 : -1;
+            }
+        }
+        int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        while (!d.isEmpty()) {
+            int[] info = d.pollFirst();
+            int x = info[0], y = info[1];
+            for (int[] di : dirs) {
+                int nx = x + di[0], ny = y + di[1];
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+                if (ans[nx][ny] != -1) continue;
+                ans[nx][ny] = ans[x][y] + 1;
+                d.addLast(new int[]{nx, ny});
+            }
+        }
+        return ans;
+    }
 }
