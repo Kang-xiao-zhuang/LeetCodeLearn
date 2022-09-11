b6f88db6173c5914a28e8b21f7be6d4f98aa2014
Kang-xiao-zhuang <2247830091@qq.com>
2022-09-11 11:14:28 +0800

    更新每日一题

--- src/com/zhuang/daily/twotwo/september/Solution001.java
+++ src/com/zhuang/daily/twotwo/september/Solution001.java
@@ -1,5 +1,8 @@
 package com.zhuang.daily.twotwo.september;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 public class Solution001 {
     public static void main(String[] args) {
         String[] logs = {"d1/", "d2/", "../", "d21/", "./"};
@@ -52,5 +55,42 @@ public class Solution001 {
             root.right = trimBST(root.right, low, high);
             return root;
         }
+    }
+
+    /**
+     * https://leetcode.cn/problems/minimum-cost-to-hire-k-workers/
+     * 9.11
+     * CV官解
+     *
+     * @param quality 数组
+     * @param wage    数组
+     * @param k       k名工人
+     * @return 组成满足上述条件的付费群体所需的最小金额
+     */
+    public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+        int n = quality.length;
+        Integer[] h = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            h[i] = i;
+        }
+        Arrays.sort(h, (a, b) -> {
+            return quality[b] * wage[a] - quality[a] * wage[b];
+        });
+        double res = 1e9;
+        double totalq = 0.0;
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> b - a);
+        for (int i = 0; i < k - 1; i++) {
+            totalq += quality[h[i]];
+            pq.offer(quality[h[i]]);
+        }
+        for (int i = k - 1; i < n; i++) {
+            int idx = h[i];
+            totalq += quality[idx];
+            pq.offer(quality[idx]);
+            double totalc = ((double) wage[idx] / quality[idx]) * totalq;
+            res = Math.min(res, totalc);
+            totalq -= pq.poll();
+        }
+        return res;
     }
 }
