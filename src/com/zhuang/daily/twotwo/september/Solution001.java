1b8bdc7acef01b0ce3b154c236d8d10b735ec385
Kang-xiao-zhuang <2247830091@qq.com>
2022-09-15 09:30:17 +0800

    9.15每日一题

--- src/com/zhuang/daily/twotwo/september/Solution001.java
+++ src/com/zhuang/daily/twotwo/september/Solution001.java
@@ -196,4 +196,31 @@ public class Solution001 {
         }
         return partialSum / (n * 0.9);
     }
+
+
+    /**
+     * https://leetcode.cn/problems/bulb-switcher-ii/
+     * 9.15
+     * 题解:https://leetcode.cn/problems/bulb-switcher-ii/solution/dengp-by-capital-worker-51rb/
+     *
+     * @param n       灯泡数量
+     * @param presses 按压开关次数
+     * @return 不同可能状态
+     */
+    public int flipLights(int n, int presses) {
+        //不按开关
+        if (presses == 0) {
+            return 1;
+        }
+        //特殊情况处理
+        if (n == 1) {
+            return 2;
+        } else if (n == 2) {
+            //特殊情况
+            return presses == 1 ? 3 : 4;
+        } else {
+            //n >= 3
+            return presses == 1 ? 4 : presses == 2 ? 7 : 8;
+        }
+    }
 }
