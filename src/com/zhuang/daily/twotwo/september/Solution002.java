1b219df4183791edef22206e44c7004139153d86
Kang-xiao-zhuang <2247830091@qq.com>
2022-09-19 08:32:32 +0800

    9.19每日一题

--- src/com/zhuang/daily/twotwo/september/Solution002.java
+++ src/com/zhuang/daily/twotwo/september/Solution002.java
@@ -164,4 +164,33 @@ public class Solution002 {
     public boolean valid(int n, int x, int y) {
         return x >= 0 && x < n && y >= 0 && y < n;
     }
+
+
+    /**
+     * https://leetcode.cn/problems/sort-array-by-increasing-frequency/
+     * 9.19
+     *
+     * @param nums 整数数组
+     * @return 返回排序后的数组
+     */
+    public int[] frequencySort(int[] nums) {
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int num : nums) {
+            cnt.put(num, cnt.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        list.sort((a, b) -> {
+            int cnt1 = cnt.get(a), cnt2 = cnt.get(b);
+            return cnt1 != cnt2 ? cnt1 - cnt2 : b - a;
+        });
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            nums[i] = list.get(i);
+        }
+        return nums;
+    }
+
 }
