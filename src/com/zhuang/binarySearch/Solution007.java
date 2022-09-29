5fe9a63204ed7f92886b3e8d46f71f6fadb2d284
Kang-xiao-zhuang <2247830091@qq.com>
2022-09-29 10:00:59 +0800

    更新二分笔记

--- src/com/zhuang/binarySearch/Solution007.java
+++ src/com/zhuang/binarySearch/Solution007.java
@@ -355,4 +355,45 @@ public class Solution007 {
         }
         return result;
     }
+
+    /**
+     * https://leetcode.cn/problems/search-in-rotated-sorted-array-ii/
+     *
+     * @param nums   整数数组
+     * @param target 目标值
+     * @return 布尔
+     */
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return nums[0] == target;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[l] == nums[mid] && nums[mid] == nums[r]) {
+                ++l;
+                --r;
+            } else if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
 }
