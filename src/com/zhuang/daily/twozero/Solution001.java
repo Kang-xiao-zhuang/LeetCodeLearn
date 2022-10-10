adf16c736eb0ab6b6b212199df71113d04b51fe0
Kang-xiao-zhuang <2247830091@qq.com>
2022-10-10 15:37:33 +0800

    2020.4.3 字符串转换整数 (atoi)

--- src/com/zhuang/daily/twozero/Solution001.java
+++ src/com/zhuang/daily/twozero/Solution001.java
@@ -43,6 +43,7 @@ public class Solution001 {
 
     /**
      * https://leetcode.cn/problems/game-of-life/
+     * 2020.4.2
      *
      * @param board 包含 m × n 个格子的面板
      */
@@ -148,5 +149,70 @@ public class Solution001 {
                 }
             }
         }
+    }
+
+    /**
+     * https://leetcode.cn/problems/string-to-integer-atoi/
+     * 2020.4.3
+     *
+     * @param s 字符串
+     * @return 整数
+     */
+    public int myAtoi(String s) {
+        Automaton automaton = new Automaton();
+        for (int i = 0; i < s.length(); i++) {
+            automaton.get(s.charAt(i));
+        }
+        return (int) (automaton.sign * automaton.ans);
+    }
+
+
+    public int myAtoi2(String str) {
+        if ((str == null) || (str.length() <= 0)) {
+            return 0;
+        }
+        //正负数的最大最小值
+        int MAX = Integer.MAX_VALUE;
+        //正负数的最大最小值
+        int MIN = Integer.MIN_VALUE;
+        int res = 0;
+        int index = 0;
+        //过滤空格
+        while ((index < str.length()) && (str.charAt(index) == ' ')) {
+            index++;
+        }
+        if (index == str.length()) return 0;
+        //取正负号
+        char firstChar = str.charAt(index);
+        boolean positive = true;
+        if (!isDigit(firstChar)) {
+            if ((firstChar != '+') && (firstChar != '-')) {
+                return 0;
+            }
+            index++;
+            positive = firstChar != '-';
+        }
+        //用负数保存正负数的边界，这样不会溢出
+        //正数 -2147483647
+        //负数 -2147483648
+        int limit = positive ? (-MAX) : MIN;
+        //过滤0
+        while ((index < str.length()) && (str.charAt(index) == '0')){
+            index++;
+        }
+        //取每一位,在非字符截止
+        while ((index < str.length()) && isDigit(str.charAt(index))) {
+            int digit = str.charAt(index++) - '0';
+            if (res < ((limit + digit) / 10)) {
+                return positive ? MAX : MIN;
+            }
+            //这里的res>=limit
+            res = (res * 10) - digit; //用减法
+        }
+        return positive ? (-res) : res;
+    }
+
+    public boolean isDigit(char c) {
+        return (c >= '0') && (c <= '9');
     }
 }
