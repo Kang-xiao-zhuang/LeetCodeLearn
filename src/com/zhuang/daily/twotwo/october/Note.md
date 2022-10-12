70159da3432a67c966045e859eed347bbb80c2d3
Kang-xiao-zhuang <2247830091@qq.com>
2022-10-12 09:14:25 +0800

    2022.10.12 链表组件

--- src/com/zhuang/daily/twotwo/october/Note.md
+++ src/com/zhuang/daily/twotwo/october/Note.md
@@ -940,3 +940,119 @@ class Solution {
 ```
 
 ![在这里插入图片描述](https://img-blog.csdnimg.cn/1a0d68e3ac4e4524bdab799f3ff6bccd.png)
+
+#### [817. 链表组件](https://leetcode.cn/problems/linked-list-components/)
+
+难度中等119
+
+给定链表头结点 `head`，该链表上的每个结点都有一个 **唯一的整型值** 。同时给定列表 `nums`，该列表是上述链表中整型值的一个子集。
+
+返回列表 `nums` 中组件的个数，这里对组件的定义为：链表中一段最长连续结点的值（该值必须在列表 `nums` 中）构成的集合。
+
+ 
+
+**示例 1：**
+
+![img](https://assets.leetcode.com/uploads/2021/07/22/lc-linkedlistcom1.jpg)
+
+```
+输入: head = [0,1,2,3], nums = [0,1,3]
+输出: 2
+解释: 链表中,0 和 1 是相连接的，且 nums 中不包含 2，所以 [0, 1] 是 nums 的一个组件，同理 [3] 也是一个组件，故返回 2。
+```
+
+**示例 2：**
+
+ ![img](https://assets.leetcode.com/uploads/2021/07/22/lc-linkedlistcom2.jpg)
+
+```
+输入: head = [0,1,2,3,4], nums = [0,3,1,4]
+输出: 2
+解释: 链表中，0 和 1 是相连接的，3 和 4 是相连接的，所以 [0, 1] 和 [3, 4] 是两个组件，故返回 2。
+```
+
+ 
+
+**提示：**
+
+- 链表中节点数为`n`
+- `1 <= n <= 104`
+- `0 <= Node.val < n`
+- `Node.val` 中所有值 **不同**
+- `1 <= nums.length <= n`
+- `0 <= nums[i] < n`
+- `nums` 中所有值 **不同**
+
+**哈希**
+
+```java
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        boolean inSet=false;
+        int ans=0;
+        while (head!=null){
+            if (set.contains(head.val)){
+                if (!inSet){
+                    inSet=true;
+                    ans++;
+                }
+            }else {
+                inSet=false;
+            }
+            head=head.next;
+        }
+        return ans;
+    }
+}
+```
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/2aae07c13cec4c9693c6219ebf4ab425.png)
+
+**虚拟节点**
+
+```java
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int ans = 0;
+        ListNode cur = new ListNode(-1, head);
+        while (head != null) {
+            if (set.contains(head.val) && !set.contains(cur.val)) {
+                ans++;
+            }
+            cur = head;
+            head = head.next;
+        }
+        return ans;
+    }
+}
+```
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/d7aab83651cf45b496aafd79eb10e3e3.png)
--- src/com/zhuang/daily/twotwo/october/Solution002.java
+++ src/com/zhuang/daily/twotwo/october/Solution002.java
@@ -1,6 +1,7 @@
 package com.zhuang.daily.twotwo.october;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -85,112 +86,49 @@ public class Solution002 {
     }
 
     /**
-     * https://leetcode.cn/problems/trapping-rain-water/
-     * 2020.4.3
+     * https://leetcode.cn/problems/linked-list-components/
+     * 2022.10.12
      *
-     * @param height 高度
-     * @return 接多少雨水
+     * @param head 头节点
+     * @param nums 列表
+     * @return 列表 nums 中组件的个数
      */
-    public int trap(int[] height) {
-        int n = height.length;
-        if (n == 0) {
-            return 0;
+    public int numComponents(ListNode head, int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
         }
-
-        int[] leftMax = new int[n];
-        leftMax[0] = height[0];
-        for (int i = 1; i < n; ++i) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
-        }
-
-        int[] rightMax = new int[n];
-        rightMax[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; --i) {
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
-        }
-
+        boolean inSet = false;
         int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        while (head != null) {
+            if (set.contains(head.val)) {
+                if (!inSet) {
+                    inSet = true;
+                    ans++;
+                }
+            } else {
+                inSet = false;
+            }
+            head = head.next;
         }
         return ans;
     }
 
-    /**
-     * https://leetcode.cn/problems/edit-distance/
-     * 2020.4.6
-     *
-     * @param word1 字符串
-     * @param word2 字符串
-     * @return int
-     */
-    public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
 
-        // 有一个字符串为空串
-        if (n * m == 0) {
-            return n + m;
+    public int numComponents2(ListNode head, int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
         }
-
-        // DP 数组
-        int[][] D = new int[n + 1][m + 1];
-
-        // 边界状态初始化
-        for (int i = 0; i < n + 1; i++) {
-            D[i][0] = i;
-        }
-        for (int j = 0; j < m + 1; j++) {
-            D[0][j] = j;
-        }
-
-        // 计算所有 DP 值
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < m + 1; j++) {
-                int left = D[i - 1][j] + 1;
-                int down = D[i][j - 1] + 1;
-                int left_down = D[i - 1][j - 1];
-                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
-                    left_down += 1;
-                }
-                D[i][j] = Math.min(left, Math.min(down, left_down));
+        int ans = 0;
+        ListNode cur = new ListNode(-1, head);
+        while (head != null) {
+            if (set.contains(head.val) && !set.contains(cur.val)) {
+                ans++;
             }
+            cur = head;
+            head = head.next;
         }
-        return D[n][m];
-    }
-
-    /**
-     * https://leetcode.cn/problems/rotate-matrix-lcci/
-     * 2020.4.7
-     *
-     * @param matrix 二维数组
-     */
-    public void rotate(int[][] matrix) {
-        //获取数组长度
-        int n = matrix.length;
-        //定义新的数组
-        int[][] matrix_new = new int[n][n];
-        //两次循环
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                //翻转后的元素的规律是 假如元素是第二列第一个，翻转后会是第二行最后一个！
-                /*
-                1 2 3    7 4 1
-                4 5 6 -> 8 5 2
-                7 8 9    9 6 3
-
-             以 2 为例
-             翻转前 位置 [0][1]
-             翻转后 位置 [1][2]
-                */
-                matrix_new[j][n - i - 1] = matrix[i][j];
-            }
-        }
-        //遍历完成之后，再将matrix_new中的结果复制到原数组中即可
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = matrix_new[i][j];
-            }
-        }
+        return ans;
     }
 }
