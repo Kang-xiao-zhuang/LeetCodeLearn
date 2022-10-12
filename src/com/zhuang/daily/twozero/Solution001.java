d6c9b1af295d5957c38b79e6091d4a7a122d5ed1
Kang-xiao-zhuang <2247830091@qq.com>
2022-10-12 09:15:44 +0800

    2020.4.3-4.7每日一题

--- src/com/zhuang/daily/twozero/Solution001.java
+++ src/com/zhuang/daily/twozero/Solution001.java
@@ -197,7 +197,7 @@ public class Solution001 {
         //负数 -2147483648
         int limit = positive ? (-MAX) : MIN;
         //过滤0
-        while ((index < str.length()) && (str.charAt(index) == '0')){
+        while ((index < str.length()) && (str.charAt(index) == '0')) {
             index++;
         }
         //取每一位,在非字符截止
@@ -214,5 +214,116 @@ public class Solution001 {
 
     public boolean isDigit(char c) {
         return (c >= '0') && (c <= '9');
+    }
+
+
+    /**
+     * https://leetcode.cn/problems/trapping-rain-water/
+     * 2020.4.3
+     *
+     * @param height 高度
+     * @return 接多少雨水
+     */
+    public int trap(int[] height) {
+        int n = height.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; ++i) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return ans;
+    }
+
+    /**
+     * https://leetcode.cn/problems/edit-distance/
+     * 2020.4.6
+     *
+     * @param word1 字符串
+     * @param word2 字符串
+     * @return int
+     */
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        // 有一个字符串为空串
+        if (n * m == 0) {
+            return n + m;
+        }
+
+        // DP 数组
+        int[][] D = new int[n + 1][m + 1];
+
+        // 边界状态初始化
+        for (int i = 0; i < n + 1; i++) {
+            D[i][0] = i;
+        }
+        for (int j = 0; j < m + 1; j++) {
+            D[0][j] = j;
+        }
+
+        // 计算所有 DP 值
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                int left = D[i - 1][j] + 1;
+                int down = D[i][j - 1] + 1;
+                int left_down = D[i - 1][j - 1];
+                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
+                    left_down += 1;
+                }
+                D[i][j] = Math.min(left, Math.min(down, left_down));
+            }
+        }
+        return D[n][m];
+    }
+
+    /**
+     * https://leetcode.cn/problems/rotate-matrix-lcci/
+     * 2020.4.7
+     *
+     * @param matrix 二维数组
+     */
+    public void rotate(int[][] matrix) {
+        //获取数组长度
+        int n = matrix.length;
+        //定义新的数组
+        int[][] matrix_new = new int[n][n];
+        //两次循环
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                //翻转后的元素的规律是 假如元素是第二列第一个，翻转后会是第二行最后一个！
+                /*
+                1 2 3    7 4 1
+                4 5 6 -> 8 5 2
+                7 8 9    9 6 3
+
+             以 2 为例
+             翻转前 位置 [0][1]
+             翻转后 位置 [1][2]
+                */
+                matrix_new[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        //遍历完成之后，再将matrix_new中的结果复制到原数组中即可
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = matrix_new[i][j];
+            }
+        }
     }
 }
