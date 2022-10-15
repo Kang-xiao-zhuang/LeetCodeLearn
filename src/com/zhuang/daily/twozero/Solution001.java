2a5d5833fcc532cabff248697d659a87344a59d9
Kang-xiao-zhuang <2247830091@qq.com>
2022-10-15 22:22:48 +0800

    2020.4.16 合并区间

--- src/com/zhuang/daily/twozero/Solution001.java
+++ src/com/zhuang/daily/twozero/Solution001.java
@@ -626,4 +626,29 @@ public class Solution001 {
         }
         return mat;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/merge-intervals/
+     * 2020.4.16
+     *
+     * @param intervals 若干区间的集合
+     * @return 一个不重叠的区间数组
+     */
+    public static int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][2];
+        }
+        Arrays.sort(intervals, (v1, v2) -> v1[0] - v2[0]);
+        List<int[]> merged = new ArrayList<>();
+        for (int i = 0; i < intervals.length; ++i) {
+            int L = intervals[i][0], R = intervals[i][1];
+            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
+                merged.add(new int[]{L, R});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
+            }
+        }
+        System.out.println(Arrays.deepToString(merged.toArray(new int[merged.size()][])));
+        return merged.toArray(new int[merged.size()][]);
+    }
 }
