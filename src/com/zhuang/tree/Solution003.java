5b5c9c1f0c0f53b38fedffd695c588a7a57b8ac5
Kang-xiao-zhuang <2247830091@qq.com>
2022-10-24 14:16:10 +0800

    617. 合并二叉树

--- src/com/zhuang/tree/Solution003.java
+++ src/com/zhuang/tree/Solution003.java
@@ -376,4 +376,40 @@ public class Solution003 {
     public boolean isLeaf(TreeNode node) {
         return node.left == null && node.right == null;
     }
+
+    /**
+     * https://leetcode.cn/problems/merge-two-binary-trees/
+     *
+     * @param root1 TreeNode
+     * @param root2 TreeNode
+     * @return TreeNode
+     */
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+
+        return root1;
+    }
+
+    public TreeNode mergeTrees2(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+
+        TreeNode root = new TreeNode(root1.val + root2.val);
+        root.left = mergeTrees(root1.left, root2.left);
+        root.right = mergeTrees(root1.right, root2.right);
+
+        return root;
+    }
 }
