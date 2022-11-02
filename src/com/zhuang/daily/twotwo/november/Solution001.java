7ba7b7170cf02a0bbdea1e4589524963cd6ce3bb
Kang-xiao-zhuang <2247830091@qq.com>
2022-11-02 11:18:11 +0800

    2022.11.2 1620. 网络信号最好的坐标

--- src/com/zhuang/daily/twotwo/november/Solution001.java
+++ src/com/zhuang/daily/twotwo/november/Solution001.java
@@ -42,4 +42,36 @@ public class Solution001 {
         }
         return (s1.toString()).equals(s2.toString());
     }
+
+    /**
+     * https://leetcode.cn/problems/coordinate-with-maximum-network-quality/
+     * 2022.11.2
+     *
+     * @param towers 数组
+     * @param k      整数
+     * @return 信号强度 最大的 整数 坐标点 (cx, cy)
+     */
+    public int[] bestCoordinate(int[][] towers, int k) {
+        int[][] g = new int[110][110];
+        int x = 0, y = 0, val = 0;
+        for (int[] t : towers) {
+            int a = t[0], b = t[1], q = t[2];
+            for (int i = Math.max(0, a - k); i <= a + k; i++) {
+                for (int j = Math.max(0, b - k); j <= b + k; j++) {
+                    double d = Math.sqrt((a - i) * (a - i) + (b - j) * (b - j));
+                    if (d > k) continue;
+                    g[i][j] += Math.floor(q / (1 + d));
+                    if (g[i][j] > val) {
+                        x = i;
+                        y = j;
+                        val = g[i][j];
+                    } else if (g[i][j] == val && (i < x || (i == x && j < y))) {
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+        }
+        return new int[]{x, y};
+    }
 }
