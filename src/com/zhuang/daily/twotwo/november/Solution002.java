b9af5bbaaeeb78cfbe3a55a29ac71979dbd604d6
Kang-xiao-zhuang <2247830091@qq.com>
2022-11-28 09:03:06 +0800

    2022.11.28 813. 最大平均值和的分组

--- src/com/zhuang/daily/twotwo/november/Solution002.java
+++ src/com/zhuang/daily/twotwo/november/Solution002.java
@@ -271,4 +271,32 @@ public class Solution002 {
         }
         return nums[0] >= nums[n - 1];
     }
+
+    /**
+     * https://leetcode.cn/problems/largest-sum-of-averages/
+     * 2022.11.28
+     *
+     * @param nums 数组
+     * @param k 整数
+     * @return 我们所能得到的最大 分数 是多少
+     */
+    public double largestSumOfAverages(int[] nums, int k) {
+        int n = nums.length;
+        double[] prefix = new double[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        double[][] dp = new double[n + 1][k + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i][1] = prefix[i] / i;
+        }
+        for (int j = 2; j <= k; j++) {
+            for (int i = j; i <= n; i++) {
+                for (int x = j - 1; x < i; x++) {
+                    dp[i][j] = Math.max(dp[i][j], dp[x][j - 1] + (prefix[i] - prefix[x]) / (i - x));
+                }
+            }
+        }
+        return dp[n][k];
+    }
 }
