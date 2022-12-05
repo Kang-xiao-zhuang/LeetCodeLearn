0ea7b8292d50c725b1840e9fe9da9443ed2d7abb
Kang-xiao-zhuang <2247830091@qq.com>
2022-12-05 18:01:05 +0800

    2022.12.5 1687. 从仓库到码头运输箱子

--- src/com/zhuang/daily/twotwo/december/Solution001.java
+++ src/com/zhuang/daily/twotwo/december/Solution001.java
@@ -139,4 +139,52 @@ public class Solution001 {
         dfs(toppingCosts, p + 1, curCost, target);
     }
 
+    /**
+     * https://leetcode.cn/problems/delivering-boxes-from-storage-to-ports/
+     * 2022.12.5
+     * @param boxes 箱子数组
+     * @param portsCount 整数
+     * @param maxBoxes 整数
+     * @param maxWeight 整数
+     * @return 返回将所有箱子送到相应码头的 最少行程 次数
+     */
+    public int boxDelivering(int[][] boxes, int portsCount, int maxBoxes, int maxWeight) {
+        int n = boxes.length;
+        int[] p = new int[n + 1];
+        int[] w = new int[n + 1];
+        int[] neg = new int[n + 1];
+        long[] W = new long[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            p[i] = boxes[i - 1][0];
+            w[i] = boxes[i - 1][1];
+            if (i > 1) {
+                neg[i] = neg[i - 1] + (p[i - 1] != p[i] ? 1 : 0);
+            }
+            W[i] = W[i - 1] + w[i];
+        }
+
+        Deque<Integer> opt = new ArrayDeque<>();
+        opt.offerLast(0);
+        int[] f = new int[n + 1];
+        int[] g = new int[n + 1];
+
+        for (int i = 1; i <= n; ++i) {
+            while (i - opt.peekFirst() > maxBoxes || W[i] - W[opt.peekFirst()] > maxWeight) {
+                opt.pollFirst();
+            }
+
+            f[i] = g[opt.peekFirst()] + neg[i] + 2;
+
+            if (i != n) {
+                g[i] = f[i] - neg[i + 1];
+                while (!opt.isEmpty() && g[i] <= g[opt.peekLast()]) {
+                    opt.pollLast();
+                }
+                opt.offerLast(i);
+            }
+        }
+
+        return f[n];
+    }
+
 }
