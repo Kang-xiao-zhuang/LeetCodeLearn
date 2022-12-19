3fe0bd1283aa1e84c13dc7554971069afc1e63cc
Kang-xiao-zhuang <2247830091@qq.com>
2022-12-19 10:02:21 +0800

    2022.12.19 1971. 寻找图中是否存在路径

--- src/com/zhuang/daily/twotwo/december/Solution001.java
+++ src/com/zhuang/daily/twotwo/december/Solution001.java
@@ -489,4 +489,43 @@ public class Solution001 {
         return true;
     }
 
+    /**
+     * https://leetcode.cn/problems/find-if-path-exists-in-graph/
+     * 2022.12.17
+     *
+     * @param n           整数
+     * @param edges       二维整数数组
+     * @param source      顶点
+     * @param destination 顶点
+     * @return 如果从 source 到 destination 存在 有效路径 ，则返回 true，否则返回 false
+     */
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        List<Integer>[] adj = new List[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for (int[] edge : edges) {
+            int x = edge[0], y = edge[1];
+            adj[x].add(y);
+            adj[y].add(x);
+        }
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+        queue.offer(source);
+        visited[source] = true;
+        while (!queue.isEmpty()) {
+            int vertex = queue.poll();
+            if (vertex == destination) {
+                break;
+            }
+            for (int next : adj[vertex]) {
+                if (!visited[next]) {
+                    queue.offer(next);
+                    visited[next] = true;
+                }
+            }
+        }
+        return visited[destination];
+    }
+
 }
