6f55f1088b901c2849c374bbf702ff9aa2e38cc7
Kang-xiao-zhuang <2247830091@qq.com>
2023-01-03 12:26:36 +0800

    2023.1.3 2042. 检查句子中的数字是否递增

--- src/com/zhuang/daily/twothree/january/Solution001.java
+++ src/com/zhuang/daily/twothree/january/Solution001.java
@@ -87,4 +87,31 @@ public class Solution001 {
         }
         return total;
     }
+
+    /**
+     * https://leetcode.cn/problems/check-if-numbers-are-ascending-in-a-sentence/
+     * 2023.1.3
+     *
+     * @param s 字符串
+     * @return 需要检查 s 中的 全部 数字是否从左到右严格递增
+     */
+    public boolean areNumbersAscending(String s) {
+        int pre = 0, pos = 0;
+        while (pos < s.length()) {
+            if (Character.isDigit(s.charAt(pos))) {
+                int cur = 0;
+                while (pos < s.length() && Character.isDigit(s.charAt(pos))) {
+                    cur = cur * 10 + s.charAt(pos) - '0';
+                    pos++;
+                }
+                if (cur <= pre) {
+                    return false;
+                }
+                pre = cur;
+            } else {
+                pos++;
+            }
+        }
+        return true;
+    }
 }
