c600c75ecf3d9c097c010dc2f4846bd27b0efd8f
Kang-xiao-zhuang <2247830091@qq.com>
2023-01-16 10:42:15 +0800

    2023.1.16 1813. 句子相似性 III

--- src/com/zhuang/daily/twothree/january/Solution001.java
+++ src/com/zhuang/daily/twothree/january/Solution001.java
@@ -415,4 +415,26 @@ public class Solution001 {
         }
         return minMaxGame(newNums);
     }
+
+    /**
+     * https://leetcode.cn/problems/sentence-similarity-iii/
+     * 2023.1.16
+     *
+     * @param sentence1 包含大写和小写英文字母
+     * @param sentence2 包含大写和小写英文字母
+     * @return 如果 sentence1 和 sentence2 是相似的，请你返回 true ，否则返回 false
+     */
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        String[] words1 = sentence1.split(" ");
+        String[] words2 = sentence2.split(" ");
+        int i = 0, j = 0;
+        while (i < words1.length && i < words2.length && words1[i].equals(words2[i])) {
+            i++;
+        }
+        while (j < words1.length - i && j < words2.length - i && words1[words1.length - j - 1].equals(words2[words2.length - j - 1])) {
+            j++;
+        }
+        return i + j == Math.min(words1.length, words2.length);
+    }
+
 }
