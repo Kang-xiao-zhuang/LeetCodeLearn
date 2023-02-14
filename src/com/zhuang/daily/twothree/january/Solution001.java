3629688e01a1eb1d8852faa0a6a1aecaa91bd06f
Kang-xiao-zhuang <2247830091@qq.com>
2023-02-14 09:17:55 +0800

    2023.1.19 2299. 强密码检验器 II

--- src/com/zhuang/daily/twothree/january/Solution001.java
+++ src/com/zhuang/daily/twothree/january/Solution001.java
@@ -459,4 +459,52 @@ public class Solution001 {
         }
         return res;
     }
+
+    /**
+     * https://leetcode.cn/problems/strong-password-checker-ii/
+     * 2023.1.19
+     *
+     * @param password 字符串
+     * @return 如果它是一个 强 密码，返回 true，否则返回 false
+     */
+    public boolean strongPasswordCheckerII(String password) {
+        if (password.length() < 8) {
+            return false;
+        }
+
+        Set<Character> specials = new HashSet<Character>() {{
+            add('!');
+            add('@');
+            add('#');
+            add('$');
+            add('%');
+            add('^');
+            add('&');
+            add('*');
+            add('(');
+            add(')');
+            add('-');
+            add('+');
+        }};
+        int n = password.length();
+        boolean hasLower = false, hasUpper = false, hasDigit = false, hasSpecial = false;
+        for (int i = 0; i < n; ++i) {
+            if (i != n - 1 && password.charAt(i) == password.charAt(i + 1)) {
+                return false;
+            }
+
+            char ch = password.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                hasLower = true;
+            } else if (Character.isUpperCase(ch)) {
+                hasUpper = true;
+            } else if (Character.isDigit(ch)) {
+                hasDigit = true;
+            } else if (specials.contains(ch)) {
+                hasSpecial = true;
+            }
+        }
+
+        return hasLower && hasUpper && hasDigit && hasSpecial;
+    }
 }
