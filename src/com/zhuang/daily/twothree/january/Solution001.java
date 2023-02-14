430290587c6d6ea628c8776fc182868925da113d
Kang-xiao-zhuang <2247830091@qq.com>
2023-02-14 09:42:23 +0800

    2023.1.28 1664. 生成平衡数组的方案数

--- src/com/zhuang/daily/twothree/january/Solution001.java
+++ src/com/zhuang/daily/twothree/january/Solution001.java
@@ -598,4 +598,39 @@ public class Solution001 {
         return "";
     }
 
+    /**
+     * https://leetcode.cn/problems/ways-to-make-a-fair-array/
+     * 2023.1.28
+     *
+     * @param nums 整数数组
+     * @return 返回删除操作后，剩下的数组 nums 是 平衡数组 的 方案数
+     */
+    public int waysToMakeFair(int[] nums) {
+        int odd1 = 0, even1 = 0;
+        int odd2 = 0, even2 = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if ((i & 1) != 0) {
+                odd2 += nums[i];
+            } else {
+                even2 += nums[i];
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if ((i & 1) != 0) {
+                odd2 -= nums[i];
+            } else {
+                even2 -= nums[i];
+            }
+            if (odd1 + even2 == odd2 + even1) {
+                ++res;
+            }
+            if ((i & 1) != 0) {
+                odd1 += nums[i];
+            } else {
+                even1 += nums[i];
+            }
+        }
+        return res;
+    }
 }
