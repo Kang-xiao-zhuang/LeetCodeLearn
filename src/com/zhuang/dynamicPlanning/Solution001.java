d63f52396b7e9fa4b66cd0c79c58f29e6331b247
Kang-xiao-zhuang <2247830091@qq.com>
2023-04-20 17:27:08 +0800

    跳台阶

--- src/com/zhuang/dynamicPlanning/Solution001.java
+++ src/com/zhuang/dynamicPlanning/Solution001.java
@@ -1,6 +1,9 @@
 package com.zhuang.dynamicPlanning;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Classname Solution001
  * @Description 动态规划学习
@@ -143,5 +146,52 @@ public class Solution001 {
         }
         System.out.println(second);
         return second;
+    }
+
+    //使用哈希map，充当备忘录的作用
+    Map<Integer, Integer> tempMap = new HashMap<>();
+
+    /**
+     * https://leetcode.cn/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/
+     *
+     * @param n int
+     * @return int
+     */
+    public int numWays(int n) {
+        // n = 0 也算1种
+        if (n == 0) {
+            return 1;
+        }
+        if (n <= 2) {
+            return n;
+        }
+        //先判断有没计算过，即看看备忘录有没有
+        if (tempMap.containsKey(n)) {
+            //备忘录有，即计算过，直接返回
+            return tempMap.get(n);
+        } else {
+            // 备忘录没有，即没有计算过，执行递归计算,并且把结果保存到备忘录map中，对1000000007取余（这个是leetcode题目规定的）
+            tempMap.put(n, (numWays(n - 1) + numWays(n - 2)) % 1000000007);
+            return tempMap.get(n);
+        }
+    }
+
+
+    public int numWays2(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int a = 1;
+        int b = 2;
+        int temp = 0;
+        for (int i = 3; i <= n; i++) {
+            temp = (a + b) % 1000000007;
+            a = b;
+            b = temp;
+        }
+        return temp;
     }
 }
