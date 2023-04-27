86a2ae91febda21da7227b1b8bb45971352acf2c
Kang-xiao-zhuang <2247830091@qq.com>
2023-04-27 19:20:58 +0800

    https://leetcode.cn/problems/squares-of-a-sorted-array/ 977. 有序数组的平方

--- src/com/zhuang/array/Solution014.java
+++ src/com/zhuang/array/Solution014.java
@@ -2,9 +2,9 @@ package com.zhuang.array;
 
 public class Solution014 {
     public static void main(String[] args) {
-        int[]gas = new int[]{1,2,3,4,5};
-        int[]cost =new int[]{3,4,5,1,2};
-        canCompleteCircuit(gas,cost);
+        int[] gas = new int[]{1, 2, 3, 4, 5};
+        int[] cost = new int[]{3, 4, 5, 1, 2};
+        canCompleteCircuit(gas, cost);
     }
 
     /**
@@ -28,5 +28,28 @@ public class Solution014 {
         }
         System.out.println(sum < 0 ? -1 : res);
         return sum < 0 ? -1 : res;
+    }
+
+    /**
+     * https://leetcode.cn/problems/squares-of-a-sorted-array/
+     *
+     * @param nums 非递减顺序 排序的整数数组 nums
+     * @return 返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序
+     */
+    public int[] sortedSquares(int[] nums) {
+        int index = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
+        int[] res = new int[nums.length];
+        while (left <= right) {
+            if (nums[left] * nums[left] > nums[right] * nums[right]) {
+                res[index--] = nums[left] * nums[left];
+                left++;
+            } else {
+                res[index--] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return res;
     }
 }
