18b56a320b5df24988acd8e5e1cb70dae91eb9d8
Kang-xiao-zhuang <2247830091@qq.com>
2023-07-06 08:55:18 +0800

    https://leetcode.cn/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/?envType=study-plan-v2&envId=coding-interviews

---
+++ src/com/zhuang/offer/Offer_QueueAndStack.java
@@ -0,0 +1,34 @@
+package com.zhuang.offer;
+
+import java.util.LinkedList;
+
+public class Offer_QueueAndStack {
+
+
+    static class CQueue {
+        private final LinkedList<Integer> stack1;
+        private final LinkedList<Integer> stack2;
+
+        public CQueue() {
+            stack1 = new LinkedList<>();
+            stack2 = new LinkedList<>();
+        }
+
+        public void appendTail(int value) {
+            stack1.push(value);
+        }
+
+        public int deleteHead() {
+            if (stack2.isEmpty()) {
+                if (stack1.isEmpty()) {
+                    return -1;
+                }
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+            return stack2.pop();
+        }
+    }
+
+}
