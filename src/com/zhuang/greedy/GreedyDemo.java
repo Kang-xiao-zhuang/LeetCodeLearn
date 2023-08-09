81c6fab7bb71af0218d5797f07159b4bea8bb91d
kangxiaozhuang <2247830091@qq.com>
2023-08-09 15:59:53 +0800

    更新贪心算法题目

--- src/com/zhuang/greedy/GreedyDemo.java
+++ src/com/zhuang/greedy/GreedyDemo.java
@@ -50,4 +50,51 @@ public class GreedyDemo {
         }
         return count;
     }
+
+    /**
+     * https://leetcode.cn/problems/wiggle-subsequence/
+     *
+     * @param nums 数组
+     * @return 个数
+     */
+    public int wiggleMaxLength(int[] nums) {
+        if (nums == null) return 0;
+        if (nums.length < 2) return nums.length;
+        int sum = 1;
+        Boolean direction = null;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) continue;//相同值不算摆动
+            if (nums[i] - nums[i - 1] > 0) {
+                if (direction != null && direction) continue;//上一次是正值
+                direction = true;
+            } else {
+                if (direction != null && !direction) continue;//上一次是负值
+                direction = false;
+            }
+            sum++;
+        }
+        return sum;
+    }
+
+
+    /**
+     * https://leetcode.cn/problems/maximum-subarray/description/
+     *
+     * @param nums 数组
+     * @return 最大子数组的和
+     */
+    public int maxSubArray(int[] nums) {
+        // 子数组的起始位置
+        int count = 0;
+        if (nums.length == 1) return nums[0];
+        int sum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            count += num;
+            // 取区间累计的最大值（相当于不断确定最大子序终止位置）
+            sum = Math.max(sum, count);
+            if (count <= 0) count = 0;
+        }
+        return sum;
+    }
+
 }
