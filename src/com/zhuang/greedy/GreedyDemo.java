8c957ee184aacb36ea19175f507efdbeb9532e43
kangxiaozhuang <2247830091@qq.com>
2023-08-16 16:58:45 +0800

    更新贪心算法题目

--- src/com/zhuang/greedy/GreedyDemo.java
+++ src/com/zhuang/greedy/GreedyDemo.java
@@ -193,4 +193,46 @@ public class GreedyDemo {
         }
         return true;
     }
+
+    /**
+     * https://leetcode.cn/problems/minimum-number-of-arrows-to-burst-balloons/description/
+     *
+     * @param points 整数数组
+     * @return 返回引爆所有气球所必须射出的 最小弓箭数
+     */
+    public int findMinArrowShots(int[][] points) {
+        // 根据气球直径的开始坐标从小到大排序
+        // 使用Integer内置比较方法，不会溢出
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+        int count = 1;
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > points[i - 1][1]) {
+                count++;
+            } else {
+                // 更新重叠气球最小右边界
+                points[i][1] = Math.min(points[i][1], points[i - 1][1]);
+            }
+        }
+        return count;
+    }
+
+    /**
+     * https://leetcode.cn/problems/non-overlapping-intervals/
+     *
+     * @param intervals 区间的集合
+     * @return int
+     */
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int count = 1;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i - 1][1]) {
+                intervals[i][1] = Math.min(intervals[i - 1][1], intervals[i][1]);
+                continue;
+            } else {
+                count++;
+            }
+        }
+        return intervals.length - count;
+    }
 }
